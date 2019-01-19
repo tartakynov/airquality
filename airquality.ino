5272142c488e74b3df41ce259312889e0ed5841b
Artem Tartakynov <artem.tad@gmail.com>
2019-01-19 22:52:48 +0700

    convert readings to integer

--- airquality.ino
+++ airquality.ino
@@ -1,6 +1,7 @@
 #include <LiquidCrystal_I2C.h>
 
-LiquidCrystal_I2C lcd(0x27, 20, 4); // set the LCD address to 0x27 for a 16 chars and 2 line display
+#define LIN_SIZE 16
+#define SER_SIZE 64
 
 /*
 US Standard
@@ -13,10 +14,10 @@ PM 2.5 (ug/m 3)                       AQI        Category
 250.5 - 350.4 (24hr)            301 - 400        Hazardous
 350.5 - 500.4 (24hr)            401 - 500        Hazardous
 */
-const float Cl_pm25[] = {0, 12.1, 35.5, 55.5, 150.5, 250.5, 350.5};
-const float Ch_pm25[] = {12.0, 35.4, 55.4, 150.4, 250.4, 350.4, 500.4};
-const float Il_pm25[] = {0, 51, 101, 151, 201, 301, 401};
-const float Ih_pm25[] = {50, 100, 150, 200, 300, 400, 500};
+const int Cl_pm25[] = {0, 12, 35, 55, 150, 250, 350};
+const int Ch_pm25[] = {12, 35, 55, 150, 250, 350, 500};
+const int Il_pm25[] = {0, 51, 101, 151, 201, 301, 401};
+const int Ih_pm25[] = {50, 100, 150, 200, 300, 400, 500};
 
 /*
 US Standard
@@ -29,21 +30,21 @@ PM 10 (ug/m 3)                        AQI        Category
 425 - 504 (24hr)                301 - 400        Hazardous
 505 - 604 (24hr)                401 - 500        Hazardous
 */
-const float Cl_pm10[] = {0, 55, 155, 255, 355, 425, 505};
-const float Ch_pm10[] = {54, 154, 254, 354, 424, 504, 604};
-const float Il_pm10[] = {0, 51, 101, 151, 201, 301, 401};
-const float Ih_pm10[] = {50, 100, 150, 200, 300, 400, 500};
+const int Cl_pm10[] = {0, 55, 155, 255, 355, 425, 505};
+const int Ch_pm10[] = {54, 154, 254, 354, 424, 504, 604};
+const int Il_pm10[] = {0, 51, 101, 151, 201, 301, 401};
+const int Ih_pm10[] = {50, 100, 150, 200, 300, 400, 500};
 
-#define LIN_SIZE 16
-#define SER_SIZE 64
+const LiquidCrystal_I2C lcd(0x27, 20, 4); // set the LCD address to 0x27 for a 16 chars and 2 line display
 
-char line[LIN_SIZE], serial[SER_SIZE];
+const char line[LIN_SIZE], serial[SER_SIZE];
+
 int pm25 = 0, pm10 = 0, aqi = 0;
 
-float getAQI(float c, float cl[], float ch[], float il[], float ih[], int n) {
+int getAQI(int c, int cl[], int ch[], int il[], int ih[], int n) {
   for (int i = 0; i < n; i++) {
     if (pm25 <= ch[i]) {
-      return ((ih[i] - il[i]) / (ch[i] - cl[i])) * (c - cl[i]) + il[i];
+      return (float(ih[i] - il[i]) / (ch[i] - cl[i])) * (c - cl[i]) + il[i];
     }
   }
 
@@ -59,7 +60,7 @@ void setup()
 
 void loop()
 {
-  pm25 = random(0, 100); // fake sensor input
+  pm25++; // = random(0, 100); // fake sensor input
   pm10 = 0; // fake sensor input
   aqi = max(
     getAQI(pm25, Cl_pm25, Ch_pm25, Il_pm25, Ih_pm25, 7),
@@ -83,5 +84,5 @@ void loop()
     Serial.print(serial);
   }
 
-  delay(5000);
+  delay(1000);
 }
